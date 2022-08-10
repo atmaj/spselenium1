80942fce1a445947cb5612eaf78816a9757b4eaf
Atmaj Bhushan <atmbhush@publicisgroupe.net>
2022-08-10 21:04:24 +0530

    first commit

---
+++ src/test/java/com/ea/SpringBasic/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package com.ea.SpringBasic.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+
+@CucumberOptions(
+        features = {"src/test/java/com/ea/SpringBasic/features"},
+        glue = "com.ea.SpringBasic.steps"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
